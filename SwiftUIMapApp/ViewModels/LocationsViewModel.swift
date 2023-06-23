995dbc66aba780f6e192f70a6edcf502d9595953
sharp1791 <csharpusa@gmail.com>
2023-06-23 11:51:13 -0500

    Just a backup point

--- SwiftUIMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftUIMapApp/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,37 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 final class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on the map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRedion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!    // shouldn't do this but the data, for now, is hard coded for testing - mock data
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRedion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
     }
 }
